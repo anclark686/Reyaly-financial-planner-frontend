0e7ccca44fda46fe20e9f12bbf76ea185e9f52f1
anclark686 <anclark686@gmail.com>
2023-07-24 23:29:07 -0400

    adjusting footer, removing about and contact

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,19 +1,21 @@
 <template>
   <footer>
     <p>Back to <RouterLink to="/dashboard">Dashboard</RouterLink></p>
-    <p>
-      <RouterLink to="/about">About Us</RouterLink>
-      |
-      <RouterLink to="/contact">Contact Us</RouterLink>
-    </p>
+    <div class="footerLinks">
+      <a href="https://www.ancportfolio.link" className="social-icon">Portfolio</a>
+      <a href="https://github.com/anclark686" target="_blank" rel="noreferrer">
+        <img alt="" src="./icons/github.png" class="social-icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/anclark686/" target="_blank" rel="noreferrer">
+        <img alt="" src="./icons/linkedin.png" class="social-icon" />
+      </a>
+    </div>
     <p>&copy; Reyaly Tech 2023</p>
   </footer>
 </template>
 
-<script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+<script setup lang="ts">
 
-export default {};
 </script>
 
 <style scoped>
@@ -28,5 +30,11 @@ footer {
 
 footer a:visited {
   color: white;
+}
+
+.social-icon {
+  height: 25px;
+  width: 25px;
+  margin: 5px;
 }
 </style>
