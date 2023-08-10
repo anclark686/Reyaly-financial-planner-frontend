66bb939dff5553e2077401f8ae44ce780f327479
anclark686 <anclark686@gmail.com>
2023-08-09 22:59:01 -0400

    adjusting frontend redirect link

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ app.use(
     clientId: clientId,
     authorizationParams: {
       // redirect_uri: "http://localhost:5173/dashboard",
-      redirect_uri: "https://main.d1r7v6lmapahca.amplifyapp.com/dashboard",
+      redirect_uri: "https://www.reyaly-financial-planner.link/dashboard",
     },
   })
 );
